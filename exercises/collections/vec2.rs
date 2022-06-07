7fec093d3f51de837e05333f892dfe1fd7ae7962
bill zhuang <zhuangmin@gmail.com>
2022-06-07 11:32:56 +0800

    commit some answers

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,15 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
+        if  *i  % 2 == 0 {
+            *i *= 2;
+            continue;
+        }
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,15 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(quantity:i32) -> i32 {
+    if quantity <= 40 {
+        2 * quantity
+    }else{
+        quantity
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -24,3 +29,5 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(65, price3);
 }
+
+
