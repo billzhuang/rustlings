48eda9ce022142b997e03712b20ea67177245b11
bill zhuang <zhuangmin@gmail.com>
2022-06-19 22:17:03 +0800

    commit some answer, box is quite confusing

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,25 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
+
+fn main(){
+    let result = call_abc("Foo".to_string());
+    println!("Result is {}", result);
+}
+
+fn call_abc(s: String) -> String {
+    let mut s2 = s;
+    s2 += "Bar";
+    s2
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert() {
-        assert!();
+        let result = call_abc("Foo".to_string());
+        assert!(result == "FooBar");
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,19 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
+
+fn call_abc(ori: String) -> String{
+    let mut b = ori;
+    b += "bar";
+    b
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(call_abc(String::from("foo")), String::from("foobar"));
     }
 }
